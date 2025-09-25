e62e952ddc842765d3aaf609ac373486e4b051f2
cquezada-cygnus <claudia.quezada@cygnus.cl>
2025-09-25 18:13:51 -0300

    tailwind + cygnus-icons ok

--- projects/ngx-cygnus-ui/src/lib/ngx-cygnus-ui.component.ts
+++ projects/ngx-cygnus-ui/src/lib/ngx-cygnus-ui.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { NgxCygnusIconsComponent } from '@cygnus/ngx-cygnus-icons';
 
 @Component({
   selector: 'lib-ngx-cygnus-ui',
-  imports: [],
+  imports: [NgxCygnusIconsComponent],
   template: `
       <h1 class="text-3xl font-bold underline">
         Hello ngx-cygnus-ui!
       </h1>
+
+      <section>
+        <h1>cygnus icons</h1>
+        <lib-ngx-cygnus-icons
+          [width]="'20px'"
+          [height]="'20px'"
+          [color]="'purple'"
+          [route]="'assets/icons/svg/Users/user-square.svg'"
+        />
+      </section>
   `,
   styles: ``
 })
