6accc87b5521ed19cce59324b3015de571942b28
cquezada-cygnus <claudia.quezada@cygnus.cl>
2025-10-02 13:49:32 -0300

    input floating error

--- projects/ngx-cygnus-ui/input/cygnus-input/cygnus-input.component.ts
+++ projects/ngx-cygnus-ui/input/cygnus-input/cygnus-input.component.ts
@@ -21,9 +21,11 @@ export class CygnusInputComponent {
 
   LABEL_BASE: string = 'block mb-1 text-sm font-medium text-gray-800';
 
-  LABEL_FLOATING: string = 'absolute top-0 start-0 !p-4 !pl-3 h-full text-sm truncate pointer-events-none transition ease-in-out duration-100 border border-transparent origin-[0_0] peer-disabled:opacity-50 peer-disabled:pointer-events-none peer-focus:scale-90 peer-focus:translate-x-0.5 peer-focus:-translate-y-1.5 peer-focus:text-gray-500 peer-[:not(:placeholder-shown)]:scale-90 peer-[:not(:placeholder-shown)]:translate-x-0.5 peer-[:not(:placeholder-shown)]:-translate-y-1.5 peer-[:not(:placeholder-shown)]:text-gray-500';
+  LABEL_FLOATING_BASE: string = 'absolute top-0 start-0 !p-4 !pl-3 h-full text-sm truncate pointer-events-none transition ease-in-out duration-100 border border-transparent origin-[0_0] peer-disabled:opacity-50 peer-disabled:pointer-events-none peer-focus:scale-90 peer-focus:translate-x-0.5 peer-focus:-translate-y-1.5 peer-focus:text-gray-500 peer-[:not(:placeholder-shown)]:scale-90 peer-[:not(:placeholder-shown)]:translate-x-0.5 peer-[:not(:placeholder-shown)]:-translate-y-1.5 peer-[:not(:placeholder-shown)]:text-gray-500';
 
-  LABEL_SUCCESS: string = '!text-success-600 !peer-focus:text-gray-500 !peer-[:not(:placeholder-shown)]:text-gray-500';
+  LABEL_FLOATING_SUCCESS: string = '!text-success-600 !peer-focus:text-gray-500 !peer-[:not(:placeholder-shown)]:text-gray-500';
+  LABEL_FLOATING_WARNING: string = '!text-warning-600 !peer-focus:text-gray-500 !peer-[:not(:placeholder-shown)]:text-gray-500';
+  LABEL_FLOATING_ERROR  : string = '!text-error-600 !peer-focus:text-gray-500 !peer-[:not(:placeholder-shown)]:text-gray-500';
 
   LABEL_INTERACTIVE_BASE: string = 'absolute cursor-text bg-white px-1 left-2.5 -top-2 text-xs scale-90 transition-all transform origin-left peer-focus:-top-2 peer-focus:left-2.5 peer-focus:text-xs peer-focus:scale-90 peer-placeholder-shown:scale-100 peer-placeholder-shown:top-3 peer-placeholder-shown:text-sm';
   LABEL_INTERACTIVE_COLOR_BASE   : string = 'text-gray-600 peer-focus:text-gray-600 peer-placeholder-shown:text-gray-600';
@@ -36,7 +38,7 @@ export class CygnusInputComponent {
   HINT_TEXT   : string = 'mt-1 text-sm text-gray-500';
   HINT_SUCCESS: string = 'text-success-600';
   HINT_WARNING: string = 'text-warning-600';
-  HINT_ERROR  : string = 'text-error-600';
+  HINT_ERROR  : string = '!text-error-600';
 
 
 }
