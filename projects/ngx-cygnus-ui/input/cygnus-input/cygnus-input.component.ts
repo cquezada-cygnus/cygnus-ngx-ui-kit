4fb3c0e8e9b4f78cfded7f721560f88da69cd795
cquezada-cygnus <claudia.quezada@cygnus.cl>
2025-10-02 13:32:05 -0300

    input & label floating ok

--- projects/ngx-cygnus-ui/input/cygnus-input/cygnus-input.component.ts
+++ projects/ngx-cygnus-ui/input/cygnus-input/cygnus-input.component.ts
@@ -9,8 +9,6 @@ import { Component } from '@angular/core';
 export class CygnusInputComponent {
   INPUT_BASE: string = 'px-3 py-3 block w-full text-sm transition duration-300 bg-white border rounded-lg shadow-sm placeholder:text-gray-500 text-gray-700 border-gray-300 focus:outline-none focus:ring-primary-700 focus:border-primary-700 focus:shadow';
 
-  INPUT_FLOATING: string = '!p-4 placeholder:text-transparent disabled:opacity-50 disabled:pointer-events-none focus:pt-6 focus:pb-2 .input-floating[:not(:placeholder-shown)]:pt-6 input-floating[:not(:placeholder-shown)]:pb-2 autofill:pt-6 autofill:pb-2';
-
   INPUT_SUCCESS: string = '!border-success-500 !focus:border-success-500 !focus:ring-success-500';
 
   INPUT_WARNING: string = '!border-warning-500 !focus:border-warning-500 !focus:ring-warning-500';
@@ -19,9 +17,11 @@ export class CygnusInputComponent {
 
   INPUT_DISABLED: string = 'bg-neutral-50 text-neutral-400 cursor-not-allowed disabled:opacity-60 disabled:pointer-events-none focus:outline-none focus:ring-0 focus:bg-gray-50 focus:border-gray-300 focus:opacity-60 focus:shadow-none';
 
+  INPUT_FLOATING: string = '!pb-2 !pt-6 placeholder:text-transparent disabled:opacity-50 disabled:pointer-events-none focus:pt-6 focus:pb-2 [&:not(:placeholder-shown)]:pt-6 [&:not(:placeholder-shown)]:pb-2 autofill:pt-6 autofill:pb-2';
+
   LABEL_BASE: string = 'block mb-1 text-sm font-medium text-gray-800';
 
-  LABEL_FLOATING: string = 'absolute top-0 start-0 !p-4 h-full text-sm truncate pointer-events-none transition ease-in-out duration-100 border border-transparent origin-[0_0] peer-disabled:opacity-50 peer-disabled:pointer-events-none peer-focus:scale-90 peer-focus:translate-x-0.5 peer-focus:-translate-y-1.5 peer-focus:text-gray-500 peer-[:not(:placeholder-shown)]:scale-90 peer-[:not(:placeholder-shown)]:translate-x-0.5 peer-[:not(:placeholder-shown)]:-translate-y-1.5 peer-[:not(:placeholder-shown)]:text-gray-500';
+  LABEL_FLOATING: string = 'absolute top-0 start-0 !p-4 !pl-3 h-full text-sm truncate pointer-events-none transition ease-in-out duration-100 border border-transparent origin-[0_0] peer-disabled:opacity-50 peer-disabled:pointer-events-none peer-focus:scale-90 peer-focus:translate-x-0.5 peer-focus:-translate-y-1.5 peer-focus:text-gray-500 peer-[:not(:placeholder-shown)]:scale-90 peer-[:not(:placeholder-shown)]:translate-x-0.5 peer-[:not(:placeholder-shown)]:-translate-y-1.5 peer-[:not(:placeholder-shown)]:text-gray-500';
 
   LABEL_SUCCESS: string = '!text-success-600 !peer-focus:text-gray-500 !peer-[:not(:placeholder-shown)]:text-gray-500';
 
