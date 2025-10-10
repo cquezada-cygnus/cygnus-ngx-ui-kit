e301acdb15077ea570ede3b94c510d369a114d85
cquezada-cygnus <claudia.quezada@cygnus.cl>
2025-10-10 10:31:02 -0300

    btn con badge

--- projects/showcase/src/app/badge-content/badge-content.component.ts
+++ projects/showcase/src/app/badge-content/badge-content.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { CygnusBadgeComponent } from 'ngx-cygnus-ui/components/badge';
+import { CygnusButtonComponent } from 'ngx-cygnus-ui/components/button';
 
 @Component({
   selector: 'app-badge-content',
-  imports: [CygnusBadgeComponent],
+  imports: [
+    CygnusBadgeComponent,
+    CygnusButtonComponent,
+  ],
   templateUrl: './badge-content.component.html',
   styleUrl: './badge-content.component.scss'
 })
--- projects/showcase/src/app/button-content/button-content.component.ts
+++ projects/showcase/src/app/button-content/button-content.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { CygnusButtonComponent, CygnusButtonLinkComponent } from 'ngx-cygnus-ui/components/button';
 import { RouterLink } from '@angular/router';
+import { CygnusBadgeComponent } from 'ngx-cygnus-ui/components/badge';
 
 @Component({
   selector: 'app-button-content',
   imports: [
     CygnusButtonComponent,
     CygnusButtonLinkComponent,
-    RouterLink
+    RouterLink,
+    CygnusBadgeComponent,
 ],
   templateUrl: './button-content.component.html',
   styleUrl: './button-content.component.scss'
