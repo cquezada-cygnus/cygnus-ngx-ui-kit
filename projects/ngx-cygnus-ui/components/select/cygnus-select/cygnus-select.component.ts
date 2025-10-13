9266baefc0c88ca0b66a175e9010438817a3bdbf
cquezada-cygnus <claudia.quezada@cygnus.cl>
2025-10-13 15:43:44 -0300

    cygnus select size

--- projects/ngx-cygnus-ui/components/select/cygnus-select/cygnus-select.component.ts
+++ projects/ngx-cygnus-ui/components/select/cygnus-select/cygnus-select.component.ts
@@ -6,6 +6,15 @@ import { TW_CLASS } from '../const/tailwind.const';
   selector: 'cygnus-select',
   imports: [],
   templateUrl: './cygnus-select.component.html',
+  styles: `select {
+    background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 20 20'%3e%3cpath stroke='%23667085' stroke-linecap='round' stroke-linejoin='round' stroke-width='1.5' d='M6 8l4 4 4-4'/%3e%3c/svg%3e");
+    background-position: right 0.5rem center;
+    background-repeat: no-repeat;
+    background-size: 1.5em 1.5em;
+    padding-right: 2.5rem;
+    -webkit-print-color-adjust: exact;
+            print-color-adjust: exact;
+  }`
 })
 export class CygnusSelectComponent {
   TW_CLASS = TW_CLASS;
@@ -17,8 +26,6 @@ export class CygnusSelectComponent {
     switch (this.selSize()) {
       case 'xs':
         return this.TW_CLASS.SELECT_XS;
-      case 'md':
-        return this.TW_CLASS.SELECT_MD;
       case 'lg':
         return this.TW_CLASS.SELECT_LG;
       default:
