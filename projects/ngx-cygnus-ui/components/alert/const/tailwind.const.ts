904b4b308647765c2db6e73abf32d90fbc2a7b2a
cquezada-cygnus <claudia.quezada@cygnus.cl>
2025-10-22 11:03:13 -0300

    alert-simple types

--- projects/ngx-cygnus-ui/components/alert/const/tailwind.const.ts
+++ projects/ngx-cygnus-ui/components/alert/const/tailwind.const.ts
@@ -1,5 +1,8 @@
 export const TW_CLASS = {
   ALERT_SIMPLE_BASE: 'w-full p-4 mb-4 text-sm rounded-lg',
   ALERT_SIMPLE_PRIMARY: 'text-blue-800 bg-blue-50',
-
+  ALERT_SIMPLE_RED: 'text-red-700 bg-red-50',
+  ALERT_SIMPLE_GREEN: 'text-green-700 bg-green-50',
+  ALERT_SIMPLE_YELLOW: 'text-yellow-700 bg-yellow-50',
+  ALERT_SIMPLE_GRAY: 'text-gray-700 bg-gray-50',
 }
--- projects/ngx-cygnus-ui/components/alert/cygnus-alert-simple/cygnus-alert-simple.component.ts
+++ projects/ngx-cygnus-ui/components/alert/cygnus-alert-simple/cygnus-alert-simple.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, input, OnInit, signal } from '@angular/core';
 import { NgxCygnusIconsComponent } from '@cygnus/ngx-cygnus-icons';
 import { TW_CLASS } from '../const/tailwind.const';
 
@@ -13,9 +13,47 @@ import { TW_CLASS } from '../const/tailwind.const';
     }
   `,
 })
-export class CygnusAlertSimpleComponent {
+export class CygnusAlertSimpleComponent implements OnInit {
   TW_CLASS = TW_CLASS;
 
   alertTitle = input<string>('');
   alertContent = input<string>('');
+
+  alertTypes = input<string>('');
+  alertAllClasses = signal<string>('');
+
+  ngOnInit(){
+    const setClasses = this.setAlertClasses(this.getAlertClasses(this.alertTypes()));
+    this.alertAllClasses.set(setClasses);
+  }
+
+  getAlertClasses(stringClasses: string): string[] {
+    return stringClasses.split(' ');
+  }
+
+  setAlertClasses(arrStringClasses: string[]): string {
+    let stringClasses = '';
+    for (let i = 0; i < arrStringClasses.length; i++) {
+      const elem = arrStringClasses[i];
+      stringClasses = stringClasses + (this.addTailwindClasses(elem) + ' ');
+    }
+    return stringClasses;
+  }
+
+  addTailwindClasses(customClass: string): string {
+    switch (customClass) {
+      case 'alert-primary':
+        return this.TW_CLASS.ALERT_SIMPLE_PRIMARY;
+      case 'alert-red':
+        return this.TW_CLASS.ALERT_SIMPLE_RED;
+      case 'alert-green':
+        return this.TW_CLASS.ALERT_SIMPLE_GREEN;
+      case 'alert-yellow':
+        return this.TW_CLASS.ALERT_SIMPLE_YELLOW;
+      case 'alert-gray':
+        return this.TW_CLASS.ALERT_SIMPLE_GRAY;
+      default:
+        return this.TW_CLASS.ALERT_SIMPLE_PRIMARY;
+    }
+  }
 }
