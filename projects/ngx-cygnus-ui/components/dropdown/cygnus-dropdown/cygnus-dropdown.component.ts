5b9e4a8e181215fda684dd97113cb23ef7f7eaa8
cquezada-cygnus <claudia.quezada@cygnus.cl>
2025-10-24 17:10:30 -0300

    cygnus-dropdown with visible/invisible class. Dropdown user menu bug

--- projects/ngx-cygnus-ui/components/dropdown/cygnus-dropdown/cygnus-dropdown.component.ts
+++ projects/ngx-cygnus-ui/components/dropdown/cygnus-dropdown/cygnus-dropdown.component.ts
@@ -1,5 +1,4 @@
-import { Component, input } from '@angular/core';
-import { trigger, state, style, animate, transition } from '@angular/animations';
+import { Component, input, signal } from '@angular/core';
 import { CygnusButtonComponent } from 'ngx-cygnus-ui/components/button';
 import { DropdownItemType } from 'ngx-cygnus-ui/types';
 import { DropdownItemData } from 'ngx-cygnus-ui/interfaces';
@@ -12,37 +11,15 @@ import { CygnusDrowpdownItemComponent } from '../cygnus-drowpdown-item/cygnus-dr
     CygnusDrowpdownItemComponent,
   ],
   templateUrl: './cygnus-dropdown.component.html',
-  styles: `
-    .dropdown-content {
-      overflow: hidden; /* Important for height animation */
-    }
-  `,
-  animations: [
-    trigger('slideInOut', [
-      state('in', style({
-        height: '*',
-        opacity: 1,
-        transform: 'translateY(0)'
-      })),
-      state('out', style({
-        height: '0px',
-        opacity: 0,
-        transform: 'translateY(-100%)'
-      })),
-      transition('in => out', animate('300ms ease-out')),
-      transition('out => in', animate('300ms ease-in'))
-    ])
-  ]
 })
 export class CygnusDropdownComponent {
   dropdownMenuTitle = input<string>('');
   dropdownItemType = input<DropdownItemType>('simple');
   dropdownRadioIconAsset = input<string>('');
   dropdownItemDataArr = input<DropdownItemData[]>([]);
-
-  dropdownOpen = false;
+  dropdownClosed = signal<boolean>(true);
 
   toggleDropdown() {
-    this.dropdownOpen = !this.dropdownOpen;
+    this.dropdownClosed.update( current => !current );
   }
 }
