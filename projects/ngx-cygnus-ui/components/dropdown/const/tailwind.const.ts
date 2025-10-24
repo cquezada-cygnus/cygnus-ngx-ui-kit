79a85a740df0f12478e2a7464662c1d034f7b15f
cquezada-cygnus <claudia.quezada@cygnus.cl>
2025-10-24 15:21:30 -0300

    cygnus dropdown 'cerrar sesion'

--- projects/ngx-cygnus-ui/components/dropdown/const/tailwind.const.ts
+++
@@ -1,4 +0,0 @@
-export const TW_CLASS = {
-  DROPDOWN: '',
-
-}
--- projects/ngx-cygnus-ui/components/dropdown/cygnus-dropdown/cygnus-dropdown.component.ts
+++ projects/ngx-cygnus-ui/components/dropdown/cygnus-dropdown/cygnus-dropdown.component.ts
@@ -1,5 +1,4 @@
 import { Component, input } from '@angular/core';
-import { TW_CLASS } from '../const/tailwind.const';
 import { CygnusButtonComponent } from 'ngx-cygnus-ui/components/button';
 import { DropdownItemType } from 'ngx-cygnus-ui/types';
 import { DropdownItemData } from 'ngx-cygnus-ui/interfaces';
@@ -14,8 +13,6 @@ import { CygnusDrowpdownItemComponent } from '../cygnus-drowpdown-item/cygnus-dr
   templateUrl: './cygnus-dropdown.component.html',
 })
 export class CygnusDropdownComponent {
-  TW_CLASS = TW_CLASS;
-
   dropdownMenuTitle = input<string>('');
   dropdownItemType = input<DropdownItemType>('simple');
   dropdownRadioIconAsset = input<string>('');
