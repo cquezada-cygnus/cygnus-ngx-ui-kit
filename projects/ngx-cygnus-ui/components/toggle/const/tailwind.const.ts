bc032c68a7d0d2c2dfb02441382463aef613fb19
cquezada-cygnus <claudia.quezada@cygnus.cl>
2025-10-28 14:21:23 -0300

    cygnus-toggle focus/checked ring

--- projects/ngx-cygnus-ui/components/toggle/const/tailwind.const.ts
+++ projects/ngx-cygnus-ui/components/toggle/const/tailwind.const.ts
@@ -1,6 +1,6 @@
 export const TW_CLASS = {
 
-  TOGGLE_BASE: 'relative inline-block h-6 w-10 appearance-none rounded-full before:absolute before:left-0 before:top-0 before:inline-block before:h-full before:w-full before:rounded-full before:bg-slate-200 before:transition-colors before:duration-200 before:ease-in after:absolute after:left-0.5 after:top-2/4 after:h-5 after:w-5 after:-translate-y-2/4 after:rounded-full after:bg-white after:transition-transform after:duration-200 after:ease-in checked:after:translate-x-[calc(100%-4px)] dark:after:bg-white',
+  TOGGLE_BASE: 'relative inline-block h-6 w-10 appearance-none rounded-full before:absolute before:left-0 before:top-0 before:inline-block before:h-full before:w-full before:rounded-full before:bg-slate-200 before:transition-colors before:duration-200 before:ease-in after:absolute after:left-0.5 after:top-2/4 after:h-5 after:w-5 after:-translate-y-2/4 after:rounded-full after:bg-white after:transition-transform after:duration-200 after:ease-in checked:after:translate-x-[calc(100%-4px)] dark:after:bg-white focus:ring-3 focus:ring-blue-200 checked:border-blue-500',
   TOGGLE_DISABLED: 'disabled:opacity-50 bg-gray-200 border-gray-300 cursor-not-allowed',
   TOGGLE_PRIMARY: 'checked:before:bg-blue-800',
   TOGGLE_GREEN: 'checked:before:bg-[#2ec946]',
