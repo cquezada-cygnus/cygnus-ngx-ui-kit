9b0282ec3142ae429248e15cdaad935b77aaf77c
cquezada-cygnus <claudia.quezada@cygnus.cl>
2025-10-28 14:06:42 -0300

    cygnus toggle custom color

--- projects/ngx-cygnus-ui/components/toggle/cygnus-toggle/cygnus-toggle.component.ts
+++ projects/ngx-cygnus-ui/components/toggle/cygnus-toggle/cygnus-toggle.component.ts
@@ -20,12 +20,15 @@ export class CygnusToggleComponent implements OnInit {
   inputlink = input<string>('');
   inputlinkText = input<string>('');
   toggleDescription = input<string>('');
+  checkedCustomColor = input<string>(''); // ejemplo: #2ec946
+  checkedCustomColorFinal = signal<string>('');
 
   control = input<FormControl<boolean>>();
 
   ngOnInit() {
     // Generar ID único si no se proporciona
     this.toggleId.set(`cg-toggle-${++CygnusToggleComponent.idCounter}`);
+    this.setCheckedCustomColor();
   }
 
   setValue(value:boolean ) {
@@ -34,6 +37,21 @@ export class CygnusToggleComponent implements OnInit {
     this.control()?.setValue(value);
     this.control()?.markAsDirty();
     this.control()?.markAsTouched();
+  }
+
+  setCheckedCustomColor():string {
+    switch (this.checkedCustomColor()) {
+      case 'primary':
+        return TW_CLASS.TOGGLE_PRIMARY;
+      case 'green':
+        return TW_CLASS.TOGGLE_GREEN;
+      case 'red':
+        return TW_CLASS.TOGGLE_RED;
+      case 'amber':
+        return TW_CLASS.TOGGLE_AMBER;
+      default:
+        return TW_CLASS.TOGGLE_PRIMARY;
+    }
   }
 
 }
