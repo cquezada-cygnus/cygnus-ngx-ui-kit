f6c30734c3b8c45173e677af8abfb3b89dfda45e
cquezada-cygnus <claudia.quezada@cygnus.cl>
2025-11-06 15:05:11 -0300

    cygnus-tabs bug ok. Animation are not in use, we only use tailwind animation

--- projects/ngx-cygnus-ui/components/tabs/cygnus-tabs/cygnus-tabs.component.ts
+++ projects/ngx-cygnus-ui/components/tabs/cygnus-tabs/cygnus-tabs.component.ts
@@ -1,5 +1,4 @@
 import { Component, input, OnInit, signal } from '@angular/core';
-import { trigger, state, style, animate, transition } from '@angular/animations';
 import { Tab } from 'ngx-cygnus-ui/interfaces';
 import { RouterLink } from "@angular/router";
 import { NgxCygnusIconsComponent } from '@cygnus/ngx-cygnus-icons';
@@ -11,19 +10,7 @@ import { NgxCygnusIconsComponent } from '@cygnus/ngx-cygnus-icons';
     NgxCygnusIconsComponent,
   ],
   templateUrl: './cygnus-tabs.component.html',
-  animations: [
-    trigger('tabContent', [
-      state('inactive', style({
-        opacity: 0,
-        transform: 'translateX(-100%)'
-      })),
-      state('active', style({
-        opacity: 1,
-        transform: 'translateX(0)'
-      })),
-      transition('inactive <=> active', animate('300ms ease-in-out'))
-    ])
-  ],
+
 })
 export class CygnusTabsComponent implements OnInit {
   tabsArr = input<Tab[]>([]);
@@ -34,7 +21,9 @@ export class CygnusTabsComponent implements OnInit {
   currentTab = signal<number>(0);
 
   ngOnInit(): void {
-    this.tabInnerHTML.set(this.tabsArr()[0]?.tabInnerHTML ? (this.tabsArr()[0]?.tabInnerHTML || '') : '');
+    if (this.showTabInnerHTML()) {
+      this.tabInnerHTML.set(this.tabsArr()[0]?.tabInnerHTML ? (this.tabsArr()[0]?.tabInnerHTML || '') : '');
+    }
   }
 
   changeCurrentTab(index: number, innerHTML?: string) {
--- projects/showcase/src/app/app.config.ts
+++ projects/showcase/src/app/app.config.ts
@@ -1,7 +1,5 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'; // Import this
-
 
 import { routes } from './app.routes';
 
@@ -9,6 +7,5 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideAnimationsAsync(),
   ]
 };
