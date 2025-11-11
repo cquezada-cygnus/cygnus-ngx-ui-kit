7ec29eb881c7d49dafd4055ef753a49dcc7478d6
cquezada-cygnus <claudia.quezada@cygnus.cl>
2025-11-11 14:44:12 -0300

    cygnus-custom-table pagination works

--- projects/ngx-cygnus-ui/components/table/cygnus-custom-table/cygnus-custom-table.component.ts
+++ projects/ngx-cygnus-ui/components/table/cygnus-custom-table/cygnus-custom-table.component.ts
@@ -3,6 +3,7 @@ import { UpperCasePipe } from '@angular/common';
 import { TW_CLASS } from '../const/tailwind.const';
 import { CygnusBadgeComponent } from 'ngx-cygnus-ui/components/badge';
 import { CygnusButtonLinkComponent, } from 'ngx-cygnus-ui/components/button';
+import { CygnusPaginationComponent } from 'ngx-cygnus-ui/components/pagination';
 import { TableType } from 'ngx-cygnus-ui/types';
 
 @Component({
@@ -11,6 +12,7 @@ import { TableType } from 'ngx-cygnus-ui/types';
     UpperCasePipe,
     CygnusBadgeComponent,
     CygnusButtonLinkComponent,
+    CygnusPaginationComponent,
   ],
   templateUrl: './cygnus-custom-table.component.html',
 })
@@ -27,8 +29,12 @@ export class CygnusCustomTableComponent implements OnInit {
   doubleKeyUp2 = input<string>('');
   doubleKeydown = input<string>('');
 
+  currentCounter:number = 1;
+  init = signal<number>(1);
+  limit = signal<number>(3);
+  maxCounter = input<number>(0);
+
   ngOnInit(): void {
-    console.log(this.dataTable());
     this.setColumnsHead();
     for (let i = 0; i < this.dataTable().length; i++) {
       const elem = this.dataTable()[i];
@@ -36,13 +42,27 @@ export class CygnusCustomTableComponent implements OnInit {
         const key = this.columnsHead()[j];
       }
     }
-
+    if (this.maxCounter()>0) {
+      this.showContent();
+    }
   }
 
   setColumnsHead() {
     for (const key in this.dataTable()[0]) {
       this.columnsHead.update(currentItems => [...currentItems, key]);
     }
+  }
+
+  showContent() {
+    const total = this.dataTable().length;
+    const amountPerPage = Math.ceil(total / this.maxCounter());
+    this.init.set(amountPerPage*(this.currentCounter-1));
+    this.limit.set((amountPerPage*this.currentCounter)-1);
+    console.log('total: ', total);
+    console.log('amountPerPage: ', amountPerPage);
+    console.log('this.init: ', this.init());
+    console.log('this.limit: ', this.limit());
+
   }
 
 }
