313eaddabafaf22f7f12d3f37640f7379b837c2a
cquezada-cygnus <claudia.quezada@cygnus.cl>
2025-11-14 17:02:18 -0300

    cygnus-menu-search-select receive arr with categories

--- projects/ngx-cygnus-ui/components/search-select/cygnus-menu-search-select/cygnus-menu-search-select.component.ts
+++ projects/ngx-cygnus-ui/components/search-select/cygnus-menu-search-select/cygnus-menu-search-select.component.ts
@@ -1,26 +1,49 @@
-import { Component, HostListener, signal } from '@angular/core';
+import { Component, HostListener, input, OnInit, signal } from '@angular/core';
 import { NgxCygnusIconsComponent } from '@cygnus/ngx-cygnus-icons';
 import { TW_CLASS } from '../const/tailwind.const';
+import { SelectCollection } from 'ngx-cygnus-ui/interfaces';
 
 @Component({
   selector: 'cygnus-menu-search-select',
   imports: [NgxCygnusIconsComponent],
   templateUrl: './cygnus-menu-search-select.component.html',
 })
-export class CygnusMenuSearchSelectComponent {
+export class CygnusMenuSearchSelectComponent implements OnInit {
+  private static idCounter = 0;
+  drowpdownSearchId = signal<string>('');
+  drowpdownSearchBtnId = signal<string>('');
+
   TW_CLASS = TW_CLASS;
 
   isInvisible = signal<boolean>(true);
+
+  menuSearchContentArr = input<SelectCollection[]>([]);
+  menuSearchDefaultText = input<string>('Todas las categorías');
+
+  menuSearchText = signal<string>('');
+
+  ngOnInit() {
+    // Generar ID único si no se proporciona
+    this.drowpdownSearchId.set(`cg-dropdown-search-${++CygnusMenuSearchSelectComponent.idCounter}`);
+    this.drowpdownSearchBtnId.set(`cg-dropdown-search-btn-${++CygnusMenuSearchSelectComponent.idCounter}`);
+
+    this.menuSearchText.set(this.menuSearchDefaultText());
+  }
 
   toggleInvisible() {
     this.isInvisible.set(!this.isInvisible()); // invisibilizar opciones
   }
 
+  selectMenu(selected: string) {
+    this.menuSearchText.set(selected);
+    this.isInvisible.set(true); // invisibilizar opciones
+  }
+
   @HostListener('document:click', ['$event'])
   onDocumentClick(event: MouseEvent) {
     if (
-      !(event.target == document.getElementById("dropdown-div")) && // si NO se hace click en dropdown
-      !(document.getElementById("dropdown-div")?.contains(event.target as Node)) // si NO se hace click en hijos del dropdown
+      !(event.target == document.getElementById(this.drowpdownSearchId())) && // si NO se hace click en dropdown
+      !(document.getElementById(this.drowpdownSearchId())?.contains(event.target as Node)) // si NO se hace click en hijos del dropdown
     ) {
       if (!this.isInvisible()) this.isInvisible.set(true); // invisibilizar opciones
     }
--- projects/showcase/src/app/select-search-content/select-search-content.component.ts
+++ projects/showcase/src/app/select-search-content/select-search-content.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CygnusMenuSearchSelectComponent, CygnusSearchSelectComponent } from 'ngx-cygnus-ui/components/search-select';
+import { SelectCollection } from 'ngx-cygnus-ui/interfaces';
 
 @Component({
   selector: 'app-select-search-content',
@@ -11,5 +12,12 @@ import { CygnusMenuSearchSelectComponent, CygnusSearchSelectComponent } from 'ng
   styleUrl: './select-search-content.component.scss'
 })
 export class SelectSearchContentComponent {
+
+  menuSearchContentArr: SelectCollection[] = [
+    {key: 'Contratos', keyItems: [] },
+    {key: 'Reportes días', keyItems: [] },
+    {key: 'Liquidaciones', keyItems: [] },
+    {key: 'Vacaciones', keyItems: [] },
+  ];
 
 }
