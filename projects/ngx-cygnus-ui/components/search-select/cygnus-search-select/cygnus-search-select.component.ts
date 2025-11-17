6a963cc45d2a9f2b0672d773fdf4eb5b00d74422
cquezada-cygnus <claudia.quezada@cygnus.cl>
2025-11-17 14:30:08 -0300

    cygnus-search-select with search and filter

--- projects/ngx-cygnus-ui/components/search-select/cygnus-search-select/cygnus-search-select.component.ts
+++ projects/ngx-cygnus-ui/components/search-select/cygnus-search-select/cygnus-search-select.component.ts
@@ -1,15 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, input, OnInit, signal } from '@angular/core';
 import { NgxCygnusIconsComponent } from '@cygnus/ngx-cygnus-icons';
 import { TW_CLASS } from '../const/tailwind.const';
+import { ReactiveFormsModule, FormControl } from '@angular/forms';
+import { SelectGeneric } from 'ngx-cygnus-ui/interfaces';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'cygnus-search-select',
   imports: [
+    ReactiveFormsModule,
     NgxCygnusIconsComponent,
   ],
   templateUrl: './cygnus-search-select.component.html',
 })
-export class CygnusSearchSelectComponent {
+export class CygnusSearchSelectComponent implements OnInit {
   TW_CLASS = TW_CLASS;
+
+  searchControl = new FormControl('');
+  items = input<SelectGeneric[]>([]);
+  filteredItems:SelectGeneric[] = [];
+
+  placeholder = input<string>('Escribe aquí...');
+
+  ngOnInit(): void {
+    this.searchControl.valueChanges.pipe(debounceTime(150)).subscribe(value => {
+      if (value != '' && value != null && value != undefined) {
+        this.filteredItems = this.items().filter( item =>
+          item.option.toUpperCase().includes(value.toUpperCase())
+        );
+      } else {
+        this.filteredItems = [];
+      }
+    });
+  }
 
 }
--- projects/showcase/src/app/select-search-content/select-search-content.component.ts
+++ projects/showcase/src/app/select-search-content/select-search-content.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CygnusMenuSearchSelectComponent, CygnusSearchSelectComponent } from 'ngx-cygnus-ui/components/search-select';
-import { SelectCollection } from 'ngx-cygnus-ui/interfaces';
+import { SelectCollection, SelectGeneric } from 'ngx-cygnus-ui/interfaces';
 
 @Component({
   selector: 'app-select-search-content',
@@ -19,5 +19,12 @@ export class SelectSearchContentComponent {
     {key: 'Liquidaciones', keyItems: [] },
     {key: 'Vacaciones', keyItems: [] },
   ];
+
+  options: SelectGeneric[] = [
+      {option: 'Contratos', value: '0001'},
+      {option: 'Liquidaciones', value: '0002'},
+      {option: 'Vacaciones', value: '0003'},
+      {option: 'Reportes', value: '0004'},
+    ];
 
 }
