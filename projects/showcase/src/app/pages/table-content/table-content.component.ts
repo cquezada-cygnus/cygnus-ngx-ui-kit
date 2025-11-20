12775fa5ebbc82056acdde47c52036b7cc4c841c
cquezada-cygnus <claudia.quezada@cygnus.cl>
2025-11-20 14:01:33 -0300

    cygnus-custom-table examples

--- projects/showcase/src/app/pages/table-content/table-content.component.ts
+++ projects/showcase/src/app/pages/table-content/table-content.component.ts
@@ -49,6 +49,147 @@ export class TableContentComponent {
     }
   `;
 
+  customTableEMPRESAS: string = `
+    <!-- COMPONENTE: cygnus-custom-table -->
+    @if (EMPRESAS) {
+      <cygnus-custom-table
+        [dataTable]="EMPRESAS"
+        [tableType]="'stripped-hover'"
+        [maxCounter]="5"
+        [tdEditArr]="empresasEditArr"
+        (emitModifiedData)="showEditedData($event)"
+        [showSearch]="false"
+      />
+    }
+  `;
+
+  codeEmpresasEditArr: string = `
+    empresasEditArr: string[] = [
+      'NOMBRE DE FANTASIA', 'EMAIL', 'DIRECCION',
+    ];
+  `;
+
+  eventEmitModifiedData: string = `
+    emitDataModified() {
+      this.emitModifiedData.emit({edited: this.editedData, original: this.originalData } );
+      this.cancelEditedData();
+    }
+  `;
+
+  customTableCOMPANY_COLAB: string = `
+    <!-- COMPONENTE: cygnus-custom-table -->
+    @if (COMPANY_COLAB) {
+      <cygnus-custom-table
+        [dataTable]="COMPANY_COLAB"
+        [tableType]="'stripped-hover'"
+        [badgeKey]="badgeKey"
+        [showSearch]="true"
+      />
+    }
+  `;
+
+  codeBadgeKey: string = `
+    badgeKey: TableBadge = {
+      key: 'ESTADO',
+      stateActive: 'Activo',
+      stateWarning: 'Pendiente',
+      stateError: 'Bloqueado'
+    };
+  `;
+
+  customTableEMPRESAS_CLIENTES: string = `
+  <!-- COMPONENTE: cygnus-custom-table -->
+    @if (EMPRESAS_CLIENTES) {
+      <cygnus-custom-table
+        [dataTable]="EMPRESAS_CLIENTES"
+        [tableType]="'stripped-hover'"
+        [selectKeyArr]="empresasSelectArr"
+        [showSearch]="true"
+      />
+    }
+  `;
+
+  codeEmpresasSelectArr: string = `
+    empresasSelectArr:SelectCollection[]  = [
+      {key:'CLIENTES', keyItems: ['NOMBRES', 'APELLIDOS', 'EMAIL']}
+    ];
+  `;
+
+  exampleCodeEmpresasClientes: string = `
+  [
+    ...,
+    {
+      "emp_autoid": 27,
+      "RAZON SOCIAL": "INGAL INGENIERIA LIMITADA",
+      "NOMBRE DE FANTASIA": "INGAL INGENIERIA",
+      "RUT": "777589504",
+      "EMAIL": "",
+      "GIRO": "Obras de Ingenieria",
+      "DIRECCION": "Villarrica 540, Providencia",
+      "CLIENTES": [
+        {
+          "cli_autoid": 279,
+          "NOMBRES": "Diego",
+          "APELLIDOS": "Pérez Pérez",
+          "RUT": "163617145",
+          "TELEFONO": "56983609470",
+          "EMAIL": "dperez@gmail.com"
+        },
+        {
+          "cli_autoid": 280,
+          "NOMBRES": "Daniela",
+          "APELLIDOS": "López",
+          "RUT": "187369681",
+          "TELEFONO": "56957826769",
+          "EMAIL": "dtlopez@uc.lc"
+        },
+        {
+          "cli_autoid": 281,
+          "NOMBRES": "Danitza",
+          "APELLIDOS": "Jerez",
+          "RUT": "108924888",
+          "TELEFONO": "56991612534",
+          "EMAIL": "dyepez@masdayv.cl"
+        },
+        {
+          "cli_autoid": 282,
+          "NOMBRES": "Eduardo",
+          "APELLIDOS": "Ramirez",
+          "RUT": "12141516k",
+          "TELEFONO": "56984180633",
+          "EMAIL": "eduardoramirezabogado@gmail.com"
+        }
+      ]
+    },
+    ...
+  ]
+  `;
+
+  importCygnusCustomTable: string = `
+    import { Component } from '@angular/core';
+    import { SelectCollection, TableBadge, TableItem } from 'ngx-cygnus-ui/interfaces';
+    import { CygnusCustomTableComponent } from 'ngx-cygnus-ui/components/table';
+
+    @Component({
+      selector: 'app-component',
+      imports: [
+        CygnusCustomTableComponent
+      ],
+      templateUrl: './app-component.component.html',
+      styleUrl: './app-component.component.scss',
+    })
+    export class AppComponentComponent {}
+  `;
+
+  codeDataExample: string = `
+    ...
+    import * as EMPRESAS from '../../../json/EMPRESAS.json';
+    ...
+    export class AppComponentComponent {
+      EMPRESAS = JSON.parse(JSON.stringify(EMPRESAS)).default;
+    }
+  `;
+
   tableBasicArr: TableItem[] = [
     { name: 'Juan Pérez', age: '45', email: 'florencia.mardonez.@sitio.com', address: 'Argomedo 601, Santiago', state: 'Activo' },
     { name: 'Jaime Valverde', age: '27', email: 'nidia.perez@sitio.com', address: 'Polo Sur 304, Ñuñoa', state: 'Activo' },
