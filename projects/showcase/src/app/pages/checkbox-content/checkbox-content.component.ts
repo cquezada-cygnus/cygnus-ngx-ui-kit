ffbac9d3476f0a33c0bbbb3ba83223eb16df382f
cquezada-cygnus <claudia.quezada@cygnus.cl>
2025-11-25 11:09:55 -0300

    cygnus-checkbox examples

--- projects/showcase/src/app/pages/checkbox-content/checkbox-content.component.ts
+++ projects/showcase/src/app/pages/checkbox-content/checkbox-content.component.ts
@@ -1,14 +1,88 @@
 import { Component } from '@angular/core';
 import { CygnusCheckboxComponent } from 'ngx-cygnus-ui/components/checkbox';
 
+import { Highlight } from 'ngx-highlightjs';
+import { HighlightLineNumbers } from 'ngx-highlightjs/line-numbers';
+
 @Component({
   selector: 'app-checkbox-content',
   imports: [
-    CygnusCheckboxComponent
+    CygnusCheckboxComponent,
+    Highlight, HighlightLineNumbers,
   ],
   templateUrl: './checkbox-content.component.html',
   styleUrl: './checkbox-content.component.scss'
 })
 export class CheckboxContentComponent {
+
+  cygnusCheckboxTs: string = `
+    import { Component } from '@angular/core';
+    import { CygnusCheckboxComponent } from 'ngx-cygnus-ui/components/checkbox';
+
+    @Component({
+      selector: 'app-component',
+      imports: [
+        CygnusCheckboxComponent
+      ],
+      templateUrl: './app-component.component.html',
+      styleUrl: './app-component.component.scss'
+    })
+    export class AppComponentComponent {}
+  `;
+
+  cygnusCheckboxHtml: string = `
+    <cygnus-checkbox [checkboxSize]="'sm'" />
+  `;
+
+  cygnusCheckboxHtmlForm: string = `
+    ...
+    <cygnus-checkbox
+      [control]="nameForm.controls.checkboxNameFormControl"
+    />
+    ...
+  `;
+
+  cygnusCheckboxTsForm: string = `
+    import { Component, inject } from '@angular/core';
+    import { CygnusCheckboxComponent } from 'ngx-cygnus-ui/components/checkbox';
+    import { ReactiveFormsModule, Validators, NonNullableFormBuilder } from '@angular/forms';
+
+    @Component({
+      selector: 'app-component',
+      imports: [
+        CygnusCheckboxComponent,
+        ReactiveFormsModule,
+      ],
+      templateUrl: './app-component.component.html',
+      styleUrl: './app-component.component.scss'
+    })
+    export class AppComponentComponent {
+      nonNullableFb = inject(NonNullableFormBuilder);
+
+      nameForm = this.nonNullableFb.group({
+        ...
+        checkboxNameFormControl: [false, [Validators.required]],
+      });
+    }
+  `;
+
+  cygnusCheckboxSizeHtml: string = `
+    <cygnus-checkbox [checkboxSize]="'sm'" />
+
+    <cygnus-checkbox [checkboxSize]="'lg'" />
+
+  `;
+
+  cygnusCheckboxTextHtml: string = `
+    <cygnus-checkbox [checkboxType]="'text'" [checkboxText]="'Recuérdame'" />
+  `;
+
+  cygnusCheckboxTextHelpHtml: string = `
+    <cygnus-checkbox
+      [checkboxType]="'text-hint'"
+      [checkboxText]="'Recuérdame'"
+      [checkboxHint]="'Recuerda mi datos'"
+    />
+  `;
 
 }
