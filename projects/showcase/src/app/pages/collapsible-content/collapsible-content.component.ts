6295c2bd9019b77f17716c5cf2216e0ad50eb964
cquezada-cygnus <claudia.quezada@cygnus.cl>
2025-11-25 10:21:57 -0300

    cygnus-collapsible example

--- projects/showcase/src/app/pages/collapsible-content/collapsible-content.component.ts
+++ projects/showcase/src/app/pages/collapsible-content/collapsible-content.component.ts
@@ -1,14 +1,41 @@
 import { Component } from '@angular/core';
 import { CygnusCollapsibleComponent } from 'ngx-cygnus-ui/components/collapsible';
 
+import { Highlight } from 'ngx-highlightjs';
+import { HighlightLineNumbers } from 'ngx-highlightjs/line-numbers';
+
 @Component({
   selector: 'app-collapsible-content',
   imports: [
-    CygnusCollapsibleComponent
+    CygnusCollapsibleComponent,
+    Highlight, HighlightLineNumbers,
   ],
   templateUrl: './collapsible-content.component.html',
   styleUrl: './collapsible-content.component.scss'
 })
 export class CollapsibleContentComponent {
+
+  cygnusCollapsibleTs: string = `
+    import { Component } from '@angular/core';
+    import { CygnusCollapsibleComponent } from 'ngx-cygnus-ui/components/collapsible';
+
+    @Component({
+      selector: 'app-component',
+      imports: [
+        CygnusCollapsibleComponent
+      ],
+      templateUrl: './app-component.component.html',
+      styleUrl: './app-component.component.scss'
+    })
+    export class CollapsibleComponentComponent {}
+  `;
+
+  cygnusCollapsibleHtml: string = `
+    <cygnus-collapsible
+      [collapsableTitle]="'Abrir Colapsable'"
+    >
+      Utilice este colapsable hecho en Tailwind. Puedes utilizarlo para acordeón, elementos plegables y mucho más.
+    </cygnus-collapsible>
+  `;
 
 }
