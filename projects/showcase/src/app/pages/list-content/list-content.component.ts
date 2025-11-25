e4f1abc2056fd7ae821b651b9404a9068347ba05
cquezada-cygnus <claudia.quezada@cygnus.cl>
2025-11-25 16:14:20 -0300

    cygnus-list examples

--- projects/showcase/src/app/pages/list-content/list-content.component.ts
+++ projects/showcase/src/app/pages/list-content/list-content.component.ts
@@ -2,10 +2,14 @@ import { Component } from '@angular/core';
 import { CygnusListComponent } from 'ngx-cygnus-ui/components/list';
 import { ListElem } from 'ngx-cygnus-ui/interfaces';
 
+import { Highlight } from 'ngx-highlightjs';
+import { HighlightLineNumbers } from 'ngx-highlightjs/line-numbers';
+
 @Component({
   selector: 'app-list-content',
   imports: [
-    CygnusListComponent
+    CygnusListComponent,
+    Highlight, HighlightLineNumbers,
   ],
   templateUrl: './list-content.component.html',
   styleUrl: './list-content.component.scss'
@@ -28,4 +32,71 @@ export class ListContentComponent {
     {title: 'Ajustes', linkText: './', value: '', badgeContent: '9' },
     {title: 'Hoja informativa', linkText: './', value: '', badgeContent: '+99' },
   ];
+
+  cygnusListImportTs: string = `
+    import { Component } from '@angular/core';
+    import { CygnusListComponent } from 'ngx-cygnus-ui/components/list';
+    import { ListElem } from 'ngx-cygnus-ui/interfaces';
+
+    @Component({
+      selector: 'app-list-component',
+      imports: [
+        CygnusListComponent,
+      ],
+      templateUrl: './app-list-component.component.html',
+      styleUrl: './app-list-component.component.scss'
+    })
+    export class AppListComponentComponent {
+      listArr: ListElem[] = [
+        {title: 'Perfil', linkText: './', value: '' },
+        {title: 'Ajustes', linkText: './', value: '' },
+        {title: 'Hoja informativa', linkText: './', value: '' },
+      ];
+    }
+  `;
+
+  cygnusListIconsTs: string = `
+    ...
+    export class AppListComponentComponent {
+      listIconArr: ListElem[] = [
+        {title: 'Hoja informativa', linkText: './', value: '', iconAsset: 'assets/icons/svg/Alerts&Feedback/bell-01.svg' },
+        {title: 'Descargas', linkText: './', value: '', iconAsset: 'assets/icons/svg/General/download-cloud-02.svg' },
+        {title: 'Cuenta de equipo', linkText: './', value: '', iconAsset: 'assets/icons/svg/Users/users-01.svg' },
+      ];
+    }
+  `;
+
+  cygnusListBadgesTs: string = `
+    ...
+    export class AppListComponentComponent {
+      listBadgeArr: ListElem[] = [
+        {title: 'Perfil', linkText: './', value: '', badgeContent: 'Nuevo' },
+        {title: 'Ajustes', linkText: './', value: '', badgeContent: '9' },
+        {title: 'Hoja informativa', linkText: './', value: '', badgeContent: '+99' },
+      ];
+    }
+  `;
+
+  interfazListElemTs: string = `
+    export interface ListElem {
+      title: string,
+      value?: any,
+      linkText?: string,
+      iconAsset?: string,
+      badgeContent?: string,
+    }
+  `;
+
+  cygnusListHtml: string = `
+    <cygnus-list [itemsArr]="listArr" />
+  `;
+
+  cygnusListIconsHtml: string = `
+    <cygnus-list [itemsArr]="listIconArr" />
+  `;
+
+  cygnusListBadgesHtml: string = `
+    <cygnus-list [itemsArr]="listBadgeArr" />
+  `;
+
 }
