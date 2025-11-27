23ea176e65ff1e27f972c4239b017ed7dfac2ee9
cquezada-cygnus <claudia.quezada@cygnus.cl>
2025-11-27 11:57:05 -0300

    cygnus-select examples

--- projects/showcase/src/app/pages/select-content/select-content.component.ts
+++ projects/showcase/src/app/pages/select-content/select-content.component.ts
@@ -2,9 +2,15 @@ import { Component } from '@angular/core';
 import { CygnusSelectComponent } from 'ngx-cygnus-ui/components/select';
 import { SelectGeneric } from 'ngx-cygnus-ui/interfaces';
 
+import { Highlight } from 'ngx-highlightjs';
+import { HighlightLineNumbers } from 'ngx-highlightjs/line-numbers';
+
 @Component({
   selector: 'app-select-content',
-  imports: [CygnusSelectComponent],
+  imports: [
+    CygnusSelectComponent,
+    Highlight, HighlightLineNumbers,
+  ],
   templateUrl: './select-content.component.html',
   styleUrl: './select-content.component.scss'
 })
@@ -24,6 +30,90 @@ export class SelectContentComponent {
     {option: 'Tengo un problema con una plataforma', value:'bucharest'},
     {option: 'Tengo un problema de red', value:'london'},
     {option: 'Tengo un problema administrativo', value:'washington'},
-  ]
+  ];
+
+  cygnusSelImportTs: string = `
+    import { Component } from '@angular/core';
+    import { CygnusSelectComponent } from 'ngx-cygnus-ui/components/select';
+    import { SelectGeneric } from 'ngx-cygnus-ui/interfaces';
+
+    @Component({
+      selector: 'app-component',
+      imports: [
+        CygnusSelectComponent
+      ],
+      templateUrl: './app-component.component.html',
+      styleUrl: './app-component.component.scss'
+    })
+    export class AppComponentComponent {}
+  `;
+
+  cygnusSelCodeTs: string = `
+    ...
+    export class AppComponentComponent {
+      options: SelectGeneric[] = [
+        {option: 'Valor 1', value: '0001'},
+        {option: 'Valor 2', value: '0002'},
+        {option: 'Valor 3', value: '0003'},
+        {option: 'Valor 4', value: '0004'},
+        {option: 'Valor 5', value: '0005'},
+        {option: 'Valor 6', value: '0006'},
+        {option: 'Valor 7', value: '0007'},
+      ];
+    }
+  `;
+
+  cygnusSelAutoWidthTs: string = `
+    ...
+    export class AppComponentComponent {
+      optionsTestAutoWidth: SelectGeneric[] = [
+        {option: 'Tengo un problema técnico', value:'brazil'},
+        {option: 'Tengo un problema con una plataforma', value:'bucharest'},
+        {option: 'Tengo un problema de red', value:'london'},
+        {option: 'Tengo un problema administrativo', value:'washington'},
+      ];
+    }
+  `;
+
+  cygnusSelHtml: string = `
+    <cygnus-select [options]="options" />
+  `;
+
+  cygnusSelSizeHtml: string = `
+    <!-- COMPONENTE: Selector XS -->
+    <cygnus-select [options]="options" [selSize]="'xs'" />
+
+    <!-- COMPONENTE: Selector básico -->
+    <cygnus-select [options]="options" />
+
+    <!-- COMPONENTE: Selector LG -->
+    <cygnus-select [options]="options" [selSize]="'lg'" />
+  `;
+
+  cygnusSelDesHtml: string = `
+    <!-- COMPONENTE: Selector deshabilitado -->
+    <cygnus-select [options]="options" [isDisabled]="true" />
+  `;
+
+  cygnusSelValHtml: string = `
+    <!-- COMPONENTE: Selector error -->
+    <cygnus-select [options]="options" [selState]="'error'" />
+
+    <!-- COMPONENTE: Selector éxito -->
+    <cygnus-select [options]="options" [selState]="'success'" />
+  `;
+
+  cygnusSelLabelHtml: string = `
+    <!-- COMPONENTE: Selector con label -->
+    <cygnus-select
+      [options]="options"
+      [selectLabel]="'Label'"
+      [selectHint]="'Este es un texto de ayuda para el usuario.'"
+    />
+  `;
+
+  cygnusSelAutoWidthHtml: string = `
+    <cygnus-select [options]="options" />
+  `;
 
 }
