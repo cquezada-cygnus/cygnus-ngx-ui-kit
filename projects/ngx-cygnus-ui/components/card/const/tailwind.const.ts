a4543dcb2c1b8d76bf78201a8706cc66fca12817
cquezada-cygnus <claudia.quezada@cygnus.cl>
2026-01-16 11:44:00 -0300

    font Inter. Cygnus card title text with small text

--- projects/ngx-cygnus-ui/components/card/const/tailwind.const.ts
+++ projects/ngx-cygnus-ui/components/card/const/tailwind.const.ts
@@ -1,6 +1,6 @@
 export const TW_CLASS = {
   CARD: 'flex flex-col bg-white border border-gray-300 rounded-lg shadow-xs',
-  CARD_BODY: 'flex flex-col max-w-sm p-6 bg-white border border-gray-200 shadow-xs rounded-lg  hover:bg-gray-100',
+  CARD_BODY: 'flex flex-col max-w-sm bg-white border border-gray-200 shadow-xs rounded-lg  hover:bg-gray-100',
   CARD_PANEL: 'p-4 justify-center sm:h-full mx-4 sm:mx-0 sm:min-w-96',
   CARD_APP: 'text-3xl text-primary-400',
   CARD_ICON: 'flex items-center justify-center',
--- projects/ngx-cygnus-ui/components/card/cygnus-card-title-text/cygnus-card-title-text.component.ts
+++ projects/ngx-cygnus-ui/components/card/cygnus-card-title-text/cygnus-card-title-text.component.ts
@@ -1,13 +1,19 @@
 import { Component, input } from '@angular/core';
 import { TW_CLASS } from '../const/tailwind.const';
 
+import { IconColorText, IconTextSize, NgxCygnusIconsComponent } from '@cygnus/ngx-cygnus-icons';
+
 @Component({
   selector: 'cygnus-card-title-text',
-  imports: [],
+  imports: [
+    NgxCygnusIconsComponent,
+  ],
   templateUrl: './cygnus-card-title-text.component.html',
 })
 export class CygnusCardTitleTextComponent {
   TW_CLASS = TW_CLASS;
   cardTitle = input<string>('');
   cardText = input<string>('');
+  cardIcon = input<string>('');
+  sizeTitle = input<string>('');
 }
