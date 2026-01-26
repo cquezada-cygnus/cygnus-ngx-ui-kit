be1cdb2f16401ae36e8f9acfb771388bf1ab9766
cquezada-cygnus <claudia.quezada@cygnus.cl>
2026-01-26 14:15:31 -0300

    cygnus-cat-section-search-select filter without category

--- projects/ngx-cygnus-ui/components/search-select/cygnus-cat-section-search-select/cygnus-cat-section-search-select.component.ts
+++ projects/ngx-cygnus-ui/components/search-select/cygnus-cat-section-search-select/cygnus-cat-section-search-select.component.ts
@@ -1,6 +1,7 @@
 import { Component, input, OnInit, signal } from '@angular/core';
 import { ReactiveFormsModule, FormControl } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { debounceTime } from 'rxjs';
 
 import { NgxCygnusIconsComponent } from '@cygnus/ngx-cygnus-icons';
 import { CygnusAlertSimpleComponent } from 'ngx-cygnus-ui/components/alert';
@@ -21,13 +22,16 @@ import { TW_CLASS } from '../const/tailwind.const';
 export class CygnusCatSectionSearchSelectComponent implements OnInit {
   TW_CLASS = TW_CLASS;
 
-  control = input<FormControl<string>>();
+  searchControl = new FormControl('');
   isInvisible = signal<boolean>(true);
   placeholder = input<string>('Escribe aquí...');
 
   OPTGROUPDATA = input<any>();
   objCatSecOpt: Record<string, any> = {}; // A record of string keys and any values
   dataEntries: any;
+  showDataEntries: any;
+  dataFilteredEntries: any;
+  filterValue: string | null = '';
   categoryName = input<string>('');
   categorySection = input<string>('');
   categoryItem = input<string>('');
@@ -38,6 +42,7 @@ export class CygnusCatSectionSearchSelectComponent implements OnInit {
   secOpVolver: string = 'VOLVERselectCategory';
   showSecOpt = signal<boolean>(false);
   showCategories = signal<boolean>(false);
+  showWithoutCategorySelected = signal<boolean>(false);
 
   maxLengthSelection = input<number>(3);
   arrSelection: any[] = [];
@@ -54,14 +59,52 @@ export class CygnusCatSectionSearchSelectComponent implements OnInit {
     this.setCategoriesAndSections();
     // Convert the record to an array of entries (key-value pairs)
     this.dataEntries = Object.entries(this.objCatSecOpt);
+    this.showDataEntries = Object.entries(this.objCatSecOpt);
     this.dataEntries = Object.fromEntries(this.dataEntries);
-    console.log('this.dataEntries:',this.dataEntries);
+    this.showDataEntries = Object.fromEntries(this.showDataEntries);
+    // showDataEntries y dataEntries tienen exactamente los mismos datos porque showDataEntries cambiará sus datos según se elija filtrar o no por lo escrito en el input de búsqueda. Por lo cual se necesita que exista un dataEntries que no cambie. ShowDataEntries estará cambiando según el uso.
+    this.filterInputSearch();
+    console.log('showDataEntries:',this.showDataEntries);
+
   }
 
   openUl() {
     if (!this.categorySelected) { // si no está seleccionada la categoría, mostrar menú de categorías
       this.showCategories.set(true);
     }
+  }
+
+  filterInputSearch() {
+    this.searchControl.valueChanges.pipe(debounceTime(150)).subscribe(value => {
+      console.log('filterInputSearch value:', value);
+      this.filterValue = value;
+      // Si no se selecciona categoría: buscar la palabra en todas las categorías.
+      if (!this.categorySelected) {
+        // Al mostrar las opciones, se muestra su sección y cargo que coincida.
+        this.showWithoutCategorySelected.set(true);
+        this.showCategories.set(false);
+        this.showSecOpt.set(true);
+      }
+      // Si se selecciona la categoría: Busca la palabra en esa categoría
+      else {
+
+      }
+
+    });
+  }
+
+  setFilteredOptions(arrOptions:any | any[]) {
+    this.dataFilteredEntries = [];
+    for (let i = 0; i < arrOptions.length; i++) {
+      const elem = arrOptions[i];
+      if (elem.cargo.toUpperCase().includes(this.filterValue?.toUpperCase())) {
+        this.dataFilteredEntries.push(elem);
+      }
+    }
+    if (this.dataFilteredEntries.length > 0) {
+      return true;
+    }
+    return false;
   }
 
   onCategorySelected(cat: any): void {
