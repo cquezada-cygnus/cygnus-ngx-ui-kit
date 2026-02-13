f16dd4305818f6006741ca00f2a9324c8c2239ce
cquezada-cygnus <claudia.quezada@cygnus.cl>
2026-02-13 16:32:23 -0300

    select disabled option with text-gray-300 color

--- projects/ngx-cygnus-ui/components/search-select/const/tailwind.const.ts
+++ projects/ngx-cygnus-ui/components/search-select/const/tailwind.const.ts
@@ -2,7 +2,7 @@ export const TW_CLASS = {
   BTN: 'text-sm px-5 py-2.5 rounded-lg font-medium focus:ring-4 focus:outline-none text-center',
   BTN_SIMPLE: 'text-white bg-blue-600 hover:bg-blue-700 focus:ring-blue-300',
   BTN_PRIMARY: 'text-white bg-blue-600 border border-none hover:bg-blue-700 hover:text-white focus:ring-blue-300 active:bg-primary-700',
-  INPUT_BASE: 'px-4 py-3 block w-full text-sm transition duration-300 bg-white border rounded-lg shadow-sm placeholder:text-gray-500 text-gray-700 border-gray-300 focus:outline-none focus:ring-primary-700 focus:border-primary-700 focus:bg-white focus:shadow active:bg-white',
+  INPUT_BASE: 'px-4 py-3 block w-full text-sm transition duration-300 bg-white border rounded-lg shadow-sm placeholder:text-gray-300 text-gray-700 border-gray-300 focus:outline-none focus:ring-primary-700 focus:border-primary-700 focus:bg-white focus:shadow active:bg-white',
   INPUT_ERROR: '!border-error-500 !focus:border-error-500 !focus:ring-error-500 border-2 border-error-500 focus:border-error-500 focus:ring-error-500',
   INPUT_SUCCESS: '!border-green-500 !focus:border-green-500 !focus:ring-green-500 border-2 border-green-500 focus:border-green-500 focus:ring-green-500',
   DROPDOWN_MENU_FILTER: 'shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-s-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 h-full',
