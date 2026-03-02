dccada6fc991055943747267f5ca31c8643c4ce6
cquezada-cygnus <claudia.quezada@cygnus.cl>
2026-03-02 18:33:52 -0300

    cygnus-cat-section-search-select dark en parte

--- projects/ngx-cygnus-ui/components/select/const/tailwind.const.ts
+++ projects/ngx-cygnus-ui/components/select/const/tailwind.const.ts
@@ -2,17 +2,17 @@ export const TW_CLASS = {
 
   SELECT: 'w-full py-2 pl-3 pr-8 text-sm transition duration-300 bg-transparent dark:bg-gray-700 border rounded-lg shadow-xs appearance-none cursor-pointer focus:outline-hidden focus:shadow-md',
 
-  SELECT_XS: 'placeholder:text-slate-400 text-slate-700 border-slate-200 rounded-lg !py-1.5 focus:border-slate-400 hover:border-slate-400 shadow-xs focus:shadow-md',
+  SELECT_XS: 'placeholder:text-slate-400 dark:placeholder:text-slate-500 text-slate-700 dark:text-slate-50 border-slate-200 dark:border-slate-500 rounded-lg !py-1.5 focus:border-slate-400 dark:focus:border-slate-600 hover:border-slate-400 dark:hover:border-slate-600 shadow-xs focus:shadow-md',
 
   SELECT_LG: 'py-3 rounded-lg shadow-xs focus:shadow-md',
 
-  SELECT_GENERIC: 'text-gray-700 dark:text-white border-gray-300 dark:border-gray-600 placeholder:text-gray-400 dark:placeholder:text-gray-500 focus:border-gray-400 hover:border-gray-400',
+  SELECT_GENERIC: 'text-gray-700 dark:text-white border-gray-300 dark:border-gray-600 placeholder:text-gray-400 dark:placeholder:text-gray-500 focus:border-gray-400 dark:focus:border-gray-700 hover:border-gray-400 dark:hover:border-gray-700',
 
   SELECT_ERROR: 'shadow-sm !border-error-500 !placeholder:text-error-400 !focus:border-error-500 !hover:border-error-300 focus:shadow-sm !focus:ring-error-700',
 
   SELECT_SUCCESS: 'shadow-sm !border-success-500 !placeholder:text-success-400 !focus:border-success-500 !hover:border-success-300 focus:shadow-sm !focus:ring-success-700',
 
-  SELECT_WIDTH_AUTHOMATIC: '!placeholder:text-slate-400 !text-slate-700 !border-slate-200 dark:border-slate-600 rounded py-2 focus:outline-none !focus:border-slate-400 !hover:border-slate-400 shadow-sm focus:shadow-md min-w-[50px]',
+  SELECT_WIDTH_AUTHOMATIC: '!placeholder:text-slate-400 !text-slate-700 !border-slate-200 dark:border-slate-600 rounded py-2 focus:outline-none !focus:border-slate-400 !dark:focus:border-slate-700 !hover:border-slate-400 !dark:hover:border-slate-700 shadow-sm focus:shadow-md min-w-[50px]',
 
   SELECT_DISABLED: 'shadow-sm opacity-50 pointer-events-none placeholder:text-secondary-400 text-secondary-700 border-secondary-200 dark:border-gray-200 ease focus:outline-none focus:border-secondary-400 hover:border-secondary-400',
 
