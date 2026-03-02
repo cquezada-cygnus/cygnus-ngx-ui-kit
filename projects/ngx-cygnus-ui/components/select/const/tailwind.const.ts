0288dd1e62dd9932f1541119aaf81fab3d080186
cquezada-cygnus <claudia.quezada@cygnus.cl>
2026-03-02 18:01:24 -0300

    cygnus-select dark mode

--- projects/ngx-cygnus-ui/components/select/const/tailwind.const.ts
+++ projects/ngx-cygnus-ui/components/select/const/tailwind.const.ts
@@ -1,20 +1,20 @@
 export const TW_CLASS = {
 
-  SELECT: 'w-full py-2 pl-3 pr-8 text-sm transition duration-300 bg-transparent border rounded-lg shadow-xs appearance-none cursor-pointer focus:outline-hidden focus:shadow-md',
+  SELECT: 'w-full py-2 pl-3 pr-8 text-sm transition duration-300 bg-transparent dark:bg-gray-700 border rounded-lg shadow-xs appearance-none cursor-pointer focus:outline-hidden focus:shadow-md',
 
   SELECT_XS: 'placeholder:text-slate-400 text-slate-700 border-slate-200 rounded-lg !py-1.5 focus:border-slate-400 hover:border-slate-400 shadow-xs focus:shadow-md',
 
   SELECT_LG: 'py-3 rounded-lg shadow-xs focus:shadow-md',
 
-  SELECT_GENERIC: 'text-gray-700 border-gray-300 placeholder:text-gray-400 focus:border-gray-400 hover:border-gray-400',
+  SELECT_GENERIC: 'text-gray-700 dark:text-white border-gray-300 dark:border-gray-600 placeholder:text-gray-400 dark:placeholder:text-gray-500 focus:border-gray-400 hover:border-gray-400',
 
   SELECT_ERROR: 'shadow-sm !border-error-500 !placeholder:text-error-400 !focus:border-error-500 !hover:border-error-300 focus:shadow-sm !focus:ring-error-700',
 
   SELECT_SUCCESS: 'shadow-sm !border-success-500 !placeholder:text-success-400 !focus:border-success-500 !hover:border-success-300 focus:shadow-sm !focus:ring-success-700',
 
-  SELECT_WIDTH_AUTHOMATIC: '!placeholder:text-slate-400 !text-slate-700 !border-slate-200 rounded py-2 focus:outline-none !focus:border-slate-400 !hover:border-slate-400 shadow-sm focus:shadow-md min-w-[50px]',
+  SELECT_WIDTH_AUTHOMATIC: '!placeholder:text-slate-400 !text-slate-700 !border-slate-200 dark:border-slate-600 rounded py-2 focus:outline-none !focus:border-slate-400 !hover:border-slate-400 shadow-sm focus:shadow-md min-w-[50px]',
 
-  SELECT_DISABLED: 'shadow-sm opacity-50 pointer-events-none placeholder:text-secondary-400 text-secondary-700 border-secondary-200 ease focus:outline-none focus:border-secondary-400 hover:border-secondary-400',
+  SELECT_DISABLED: 'shadow-sm opacity-50 pointer-events-none placeholder:text-secondary-400 text-secondary-700 border-secondary-200 dark:border-gray-200 ease focus:outline-none focus:border-secondary-400 hover:border-secondary-400',
 
   // Wrapper para gradient border
   GRADIENT_WRAPPER: 'relative rounded-lg p-[2px] bg-gradient-to-r from-[#5850EC] to-[#F94C00]',
@@ -23,7 +23,7 @@ export const TW_CLASS = {
   // Versión sin gradient (normal)
   NORMAL_WRAPPER: 'relative',
 
-  HINT_TEXT   : 'mt-1 text-sm text-gray-500',
+  HINT_TEXT   : 'mt-1 text-sm text-gray-500 dark:text-white',
   HINT_SUCCESS: 'mt-1 text-sm !text-success-600',
   HINT_ERROR  : 'mt-1 text-sm !text-error-600',
 }
