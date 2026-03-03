9e8ad4ee70e293de15c8c200a688bccecbf245bc
cquezada-cygnus <claudia.quezada@cygnus.cl>
2026-03-03 18:11:22 -0300

    cygnus-checkbox darkmode

--- projects/ngx-cygnus-ui/components/checkbox/const/tailwind.const.ts
+++ projects/ngx-cygnus-ui/components/checkbox/const/tailwind.const.ts
@@ -3,6 +3,6 @@ export const TW_CLASS = {
   CHECKBOX_SM: '!w-4 !h-4',
   CHECKBOX_LG: '!w-6 !h-6',
   CHECKBOX_TEXT: 'inline-flex flex-col items-start justify-start grow shrink basis-0 ms-2',
-  CHECKBOX_LABEL: 'text-sm text-gray-700 font-medium block',
-  CHECKBOX_HINT: 'mt-1 text-sm text-gray-500',
+  CHECKBOX_LABEL: 'text-sm text-gray-700 dark:text-white font-medium block',
+  CHECKBOX_HINT: 'mt-1 text-xs text-gray-500 dark:text-gray-400',
 }
